150429056c50b48a53f4baeed441443180be46ab
Ivan Krasilnikov <krasivan93@gmail.com>
2021-05-10 17:52:08 +0300

    Add new method for MainPage 'should_be_login_link'

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,20 @@
+from selenium.common.exceptions import NoSuchElementException
+
+
 class BasePage:
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
-        """метод открывает нужную страницу используя метод get()"""
+        """Метод открывает нужную страницу используя метод get()"""
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        """Метод проверяет наличие элемента на странице"""
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
